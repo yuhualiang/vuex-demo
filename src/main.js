324b5412de3c8e065ced51f14b741d3e9da43a8e
mengxiang <1547719290@qq.com>
2020-01-17 23:59:16 +0800

    全局守卫

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex'
 import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+Vue.prototype.$store = store
+
+router.beforeEach((to, from, next) => {
+  if (store.state.userInfo || to.path === '/login') {
+    next()
+  } else {
+    next({
+      path: '/login'
+    })
+  }
+})
 new Vue({
   router,
   store,
